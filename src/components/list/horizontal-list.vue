ea34330eb44cf82736c6c8f4b7f42707c484b0a8
肆叶 <lofreer@gmail.com>
2020-11-11 00:05:17 +0800

    动态数据配置；

--- src/components/list/horizontal-list.vue
+++ src/components/list/horizontal-list.vue
@@ -1,39 +1,15 @@
 <template>
-  <div
-    :class="['comp-content', component.active ? 'active' : '']"
-    :style="getStyle"
-    :comp-id="component.domId"
-  >
+  <div :class="['comp-content', component.active ? 'active' : '']"
+       :style="getStyle"
+       :comp-id="component.domId">
     <div class="page-list">
-      <div
-        v-for="(item, idx) in list"
-        class="page-list-item"
-        :key="idx"
-        :style="getItemStyle('horizontal-list-item')"
-        @click="handleClick(item)"
-      >
-        <div
-          class="page-list-item__hd"
-          :style="getItemStyle('horizontal-list-logo_')"
-        >
-          <div
-            class="page-list-item__img"
-            :style="{ backgroundImage: 'url(' + item.val + ')' }"
-          ></div>
+      <div v-for="(item, idx) in list" class="page-list-item" :key="idx" :style="getItemStyle('horizontal-list-item')" @click="handleClick(item)">
+        <div class="page-list-item__hd" :style="getItemStyle('horizontal-list-logo_')">
+          <div class="page-list-item__img" :style="{backgroundImage: 'url(' + item.val + ')'}"></div>
         </div>
         <div class="page-list-item__bd">
-          <div
-            class="page-list-item__title"
-            :style="getItemStyle('horizontal-list-title_')"
-          >
-            {{ item.title }}
-          </div>
-          <p
-            class="page-list-item__desc"
-            :style="getItemStyle('horizontal-list-desc_')"
-          >
-            {{ item.desc }}
-          </p>
+          <div class="page-list-item__title" :style="getItemStyle('horizontal-list-title_')">{{ item.title }}</div>
+          <p class="page-list-item__desc" :style="getItemStyle('horizontal-list-desc_')">{{ item.desc }}</p>
         </div>
       </div>
     </div>
@@ -41,57 +17,54 @@
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
-  name: "comp-horizontal-list",
+  name: 'comp-horizontal-list',
   props: {
     component: {
-      type: Object,
-    },
+      type: Object
+    }
   },
   data() {
     return {
-      list: this.component.action.config,
-    };
+      list: this.component.action.config
+    }
   },
   computed: {
     getStyle() {
-      const ret = [];
+      const ret = []
       this.component.style.forEach((item) => {
-        const unit = item.unit || "";
+        const unit = item.unit || ''
         if (item.val) {
-          if (item.attr === "background-image") {
-            ret.push(item.attr + ":url(" + item.val + ")");
+          if (item.attr === 'background-image') {
+            ret.push(item.attr + ':url(' + item.val + ')')
           } else {
-            ret.push(item.attr + ":" + item.val + unit);
+            ret.push(item.attr + ':' + item.val + unit)
           }
         }
-      });
-      return ret.join(";");
-    },
+      })
+      return ret.join(';')
+    }
+  },
+  created() {
+    this.fetchData()
   },
   methods: {
     getItemStyle(key) {
-      const ret = [];
+      const ret = []
       this.component.others.config.forEach((item) => {
-        const isItem = item.attr.indexOf(key);
-        const idx = item.attr.indexOf("_");
+        const isItem = item.attr.indexOf(key)
+        const idx = item.attr.indexOf('_')
         if (isItem === 0) {
-          const unit = item.unit || "";
-          item.val &&
-            ret.push(
-              item.attr.substring(idx + 1, item.attr.length) +
-                ":" +
-                item.val +
-                unit
-            );
+          const unit = item.unit || ''
+          item.val && ret.push(item.attr.substring(idx + 1, item.attr.length) + ':' + item.val + unit)
         }
-      });
-      return ret.join(";");
+      })
+      return ret.join(';')
     },
     handleClick(item) {
-      if (this.$editor) {
-        return;
-      }
+      if (this.$editor) {return}
       if (item.click) {
         const { type, href } = item.click;
 
@@ -105,81 +78,130 @@ export default {
         }
       }
     },
-  },
-};
-</script>
+    fetchData() {
+      if (this.$editor) {return}
+      try {
+        if (this.component.data) {
+          const config = {}
+          this.component.data.forEach(v => {
+            config[v.attr] = v.val
+          })
+          const { url, method, headers, params, path, image, title, description, link } = config
 
-<style lang="less" scoped>
-.comp-content {
-  background-repeat: no-repeat;
-}
-.page-list {
-  background-color: #fff;
-
-  .page-list-item {
-    position: relative;
-    display: flex;
-    align-items: center;
-
-    &:after {
-      content: " ";
-      position: absolute;
-      left: 0;
-      bottom: 0;
-      right: 0;
-      height: 1px;
-      background-color: #e5e5e5;
-      -webkit-transform-origin: 0 0;
-      transform-origin: 0 0;
-      -webkit-transform: scaleY(0.5);
-      transform: scaleY(0.5);
-    }
-
-    &:last-child {
-      &:after {
-        display: none;
-      }
-    }
-
-    .page-list-item__hd {
-      margin-right: 0.8em;
-      text-align: center;
-
-      .page-list-item__img {
-        margin: 0 auto;
-        height: 100%;
-        background-color: #e9e9eb;
-        background-size: cover;
-        background-position: center;
-        background-repeat: no-repeat;
-      }
-    }
-
-    .page-list-item__bd {
-      flex: 1;
-      min-width: 60px;
-
-      .page-list-item__title {
-        margin-bottom: 0.3em;
-        width: auto;
-        overflow: hidden;
-        text-overflow: ellipsis;
-        white-space: nowrap;
-        word-wrap: normal;
-        word-wrap: break-word;
-        word-break: break-all;
-      }
-
-      .page-list-item__desc {
-        margin: 0;
-        line-height: 1.5;
-        overflow: hidden;
-        text-overflow: ellipsis;
-        display: -webkit-box;
-        -webkit-box-orient: vertical;
-        -webkit-line-clamp: 2;
+          if (!url || !method || !path || !image || !title || !description) { return }
+          const query = { method, url }
+          if (headers) {
+            query.headers = JSON.parse(headers)
+          }
+          if (params) {
+            query[method.toLowerCase() === 'post' ? 'data' : 'params'] = JSON.parse(params)
+          }
+          axios(query).then(response => {
+            const res = response.data
+            const paths = path.split('.')
+            let data = null
+            for (let [i, key] of paths.entries()) {
+              data = data ? data[key] : res[key]
+            }
+            if (!data) { return }
+            const list = []
+            const regex = /{{(.*?)}}/
+            data.forEach(item => {
+              const value = {
+                val: image.replace(regex, item[image.match(regex)[1]]),
+                title: title.replace(regex, item[title.match(regex)[1]]) || '暂无标题',
+                desc: description.replace(regex, item[description.match(regex)[1]]) || '暂无描述',
+              }
+              if (link) {
+                value.click = {
+                  href: link.replace(regex, item[link.match(regex)[1]]) || '',
+                  type: 'outside'
+                }
+              }
+              list.push(value)
+            })
+            this.list = list
+          })
+        }
+      } catch (err) {
+        console.log(err)
       }
     }
   }
 }
+</script>
+
+<style lang="less" scoped>
+  .comp-content {
+    background-repeat: no-repeat;
+  }
+  .page-list {
+    background-color: #fff;
+
+    .page-list-item {
+      position: relative;
+      display: flex;
+      align-items: center;
+
+      &:after {
+        content: " ";
+        position: absolute;
+        left: 0;
+        bottom: 0;
+        right: 0;
+        height: 1px;
+        background-color: #E5E5E5;
+        -webkit-transform-origin: 0 0;
+        transform-origin: 0 0;
+        -webkit-transform: scaleY(0.5);
+        transform: scaleY(0.5);
+      }
+
+      &:last-child {
+        &:after {
+          display: none;
+        }
+      }
+
+      .page-list-item__hd {
+        margin-right: .8em;
+        text-align: center;
+
+        .page-list-item__img {
+          margin: 0 auto;
+          height: 100%;
+          background-color: #e9e9eb;
+          background-size: cover;
+          background-position: center;
+          background-repeat: no-repeat;
+        }
+      }
+
+      .page-list-item__bd {
+        flex: 1;
+        min-width: 60px;
+
+        .page-list-item__title {
+          margin-bottom: .3em;
+          width: auto;
+          overflow: hidden;
+          text-overflow: ellipsis;
+          white-space: nowrap;
+          word-wrap: normal;
+          word-wrap: break-word;
+          word-break: break-all;
+        }
+
+        .page-list-item__desc {
+          margin: 0;
+          line-height: 1.5;
+          overflow: hidden;
+          text-overflow: ellipsis;
+          display: -webkit-box;
+          -webkit-box-orient: vertical;
+          -webkit-line-clamp: 2;
+        }
+      }
+    }
+  }
 </style>
